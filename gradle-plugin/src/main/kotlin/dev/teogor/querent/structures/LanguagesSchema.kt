7ea6607ef486096de7a66f629ff3c389f1f14091
Teodor Grigor <open-source@teogor.dev>
2024-04-16 13:39:21 +0300

    Bump Dependencies to Latest Versions

--- gradle-plugin/src/main/kotlin/dev/teogor/querent/structures/LanguagesSchema.kt
+++ gradle-plugin/src/main/kotlin/dev/teogor/querent/structures/LanguagesSchema.kt
@@ -46,7 +46,7 @@ class LanguagesSchema(data: FoundationData) : Blueprint(data) {
 
   override fun isEnabled() = buildFeatures.languagesSchema
 
-  override fun CommonExtension<*, *, *, *, *>.finalizeDsl() {
+  override fun CommonExtension<*, *, *, *, *, *>.finalizeDsl() {
     rawResourceConfig = defaultConfig.resourceConfigurations.toMutableSet()
   }
 
